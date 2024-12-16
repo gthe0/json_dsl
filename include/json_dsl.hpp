246c5683b181f1fc074a5686b699393043bf0626
Αργύρης <argyrispatramanis@gmail.com>
2024-12-16 23:49:25 +0200

    start1

--- include/json_dsl.hpp
+++ include/json_dsl.hpp
@@ -1,5 +1,44 @@
-#define KEY(name) \
-    switch (1) \
-    {\
-    case 1:\
-    case name
+#include <iostream>
+#include <list>
+#include <string>
+
+#define STRING(x) #x;
+#define INTEGER(x) x;
+#define TRUE true;
+#define FALSE false;
+//#define NULL null;
+
+#define concat(x, y) x##y
+
+#define JSON(name) JSON json; json(#name);
+
+class JSON {
+private:
+    std::string name;
+    std::string sval;
+    int ival;
+    double dval;
+    bool bval;
+    typedef struct object {
+        int ival;
+        double dval;
+        std::string sval;
+    }oval;
+    std::list<JSON> array;
+
+public:
+    static std::list<JSON> jsonList;
+
+    void display() const {
+        std::cout << name << "\n";
+    }
+
+    void operator()(std::string name) {
+        JSON json;
+        json.name = name;
+        jsonList.emplace_back(json);
+    }
+
+    
+};
+
--- src/json_dsl.cpp
+++ src/json_dsl.cpp
@@ -1,3 +1,15 @@
-#include <json_dsl.hpp>
+#include "../include/json_dsl.hpp"
 
-int main(int argc, char *argv[]) { return 0; }
+std::list<JSON> JSON::jsonList;
+
+int main(int argc, char *argv[]) { 
+    JSON(obj)
+
+    for (const auto& jsonObj : JSON::jsonList) {
+        jsonObj.display();
+    }
+
+    return 0; 
+}
+
+
