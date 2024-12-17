b0771c60356633be76477df1c803e5f6ae687ee7
gtheo <89408641+gthe0@users.noreply.github.com>
2024-12-17 22:55:32 +0200

    Made an implementation of typeof

--- include/json_dsl.hpp
+++ include/json_dsl.hpp
@@ -1,6 +1,8 @@
 #ifndef JSON_DSL_HPP
 #define JSON_DSL_HPP
 
+#include <map>
+#include <vector>
 #include <string>
 
 #define PROGRAM_BEGIN int main(void) {
@@ -12,37 +14,54 @@
 class JsonVariable {
 public:
     virtual             ~JsonVariable() = default;
-    virtual std::string typeOf()        = 0;
+    virtual std::string typeOf() const  = 0;
 
     // Mostly the same in all of the derived classes
-    virtual bool        isEmpty()       { return 1; }
-    virtual int         sizeOf()        { return 1; }
-    virtual bool        hasKey()        { return false; }
+    virtual bool        isEmpty() const { return 1; }
+    virtual int         sizeOf()  const { return 1; }
+    virtual bool        hasKey()  const { return false; }
 };
 
 class JsonString : public JsonVariable {
 public:
+    std::string typeOf() const { return "string";}
 private:
+    std::string value_;
 };
 
 class JsonNumber : public JsonVariable {
 public:
+    std::string typeOf() const { return "number";}
 private:
+    double value_;
 };
 
 class JsonBoolean : public JsonVariable {
 public:
+    std::string typeOf() const { return "boolean";}
 private:
+    bool value_;
 };
 
 class JsonObject : public JsonVariable {
 public:
+    std::string typeOf() const { return "object";}
 private:
+    std::map<std::string, JsonVariable> kvmap_;
 };
 
 class JsonArray : public JsonVariable {
 public:
+    std::string typeOf() const { return "array";}
+private:
+    std::vector<JsonVariable> array_;
+};
+
+class JsonNull : public JsonVariable {
+public:
+    std::string typeOf() const { return "null";}
 private:
 };
+
 
 #endif // json_dsl lib
