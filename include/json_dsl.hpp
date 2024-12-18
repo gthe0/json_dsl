e753501649c4f7cffa4706c9c7965b14898601c2
Αργύρης <argyrispatramanis@gmail.com>
2024-12-18 02:18:04 +0200

    start3

--- include/json_dsl.hpp
+++ include/json_dsl.hpp
@@ -4,16 +4,18 @@
 #include <vector>
 #include <stdarg.h>
 
-#define STRING(x) #x;
-#define NUMBER(x) x;
-#define TRUE true;
-#define FALSE false;
-//#define NULL nullptr;
+#define PROGRAM_BEGIN int main() { 
+#define PROGRAM_END ; return 0;}
+
+#define STRING(x) #x
+#define NUMBER(x) x
+#define TRUE true
+#define FALSE false
+//#define NULL nullptr
 
 #define concat(x, y) x##y
 
-#define JSON(name) JSON json; json(#name)
-#define OBJECT(...) createObject(__VA_ARGS__);
+#define JSON(name) ; Json name; name(#name)
 
 class OBJECT {
 private:
@@ -22,48 +24,68 @@ private:
         void* value;
     }pair;
     pair* next;
+
+public:
+    OBJECT(std::initializer_list<std::string> args) {
+        for (const auto& arg : args) {
+            std::cout << arg << std::endl;
+        }
+    }
 };
 
-class JSON {
+struct ObjectBuilder {
+    void operator=(std::initializer_list<std::string> args) {
+        OBJECT obj(args);
+    }
+};
+
+class Json {
 private:
     std::string name;
     std::string sval;
     int ival;
     double dval;
     bool bval;
-    OBJECT object;
-    std::list<JSON> array;
+    OBJECT oval();
+    std::list<Json> array;
 
 public:
-    static std::list<JSON> jsonList;
+    static std::list<Json> jsonList;
 
     void display() const {
-        std::cout << jsonList.back().bval << std::endl;
+        std::cout << jsonList.back().name << std::endl;
     }
 
-    JSON& operator()(std::string name) {
-        JSON json;
+    Json& operator()(std::string name) {
+        Json json;
         json.name = name;
         jsonList.emplace_back(json);
         return *this;
     }
 
-    void operator=(int arg) {
+    Json& operator=(int arg) {
         jsonList.back().ival = arg;
+        return *this;
     }
 
-    void operator=(std::string arg) {
+    Json& operator=(std::string arg) {
         jsonList.back().sval = arg;
+        return *this;
     }
 
-    void operator=(double arg) {
+    Json& operator=(double arg) {
         jsonList.back().dval = arg;
+        return *this;
     }
 
-    void operator=(bool arg) {
+    Json& operator=(bool arg) {
         jsonList.back().bval = arg;
+        return *this;
     }
 
-    
+    Json& operator=(OBJECT arg) {
+        //jsonList.back().oval() = arg;
+        return *this;
+    }
 };
 
--- src/json_dsl.cpp
+++ src/json_dsl.cpp
@@ -1,22 +1,10 @@
 #include "../include/json_dsl.hpp"
 
-std::list<JSON> JSON::jsonList;
+std::list<Json> Json::jsonList;
 
-template <typename... Things>
-void createObject(Things... things) {
-    for (const auto p : {things...}) {
-        std::cout << p << std::endl;
-    }
-}
-
-int main(int argc, char *argv[]) { 
+PROGRAM_BEGIN
 
     JSON(obj) = NUMBER(10)
-    OBJECT("test", "test1", "test2")
+    JSON(test) = OBJECT{"test"}
 
-    for (const auto& jsonObj : JSON::jsonList) {
-        jsonObj.display();
-    }
-
-    return 0; 
-}
+PROGRAM_END
