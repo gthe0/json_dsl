7b696bbcffe98c4d10f1f6858c83209cd16b0223
gtheo <89408641+gthe0@users.noreply.github.com>
2024-12-23 17:37:11 +0200

    Still thinking

--- include/json_dsl.hpp
+++ include/json_dsl.hpp
@@ -7,36 +7,49 @@
 
 #define PROGRAM_BEGIN int main(void) {
 #define PROGRAM_END \
-    return 0; }
+    ;return 0; }
 
-#define JSON(var)   JsonVariable var
-#define STRING(var) JsonString(var);
-#define NUMBER(var) JsonNumber(var);
+#define JSON(var)   ;JsonVariable var
+
+// Variable Values
+#define STRING(var) JsonString(var)
+#define NUMBER(var) JsonNumber(var)
+#define TRUE        JsonBoolean(true)
+#define FALSE       JsonBoolean(false)
 
 // Undef NULL whose default value is (void*)0x0
 #undef  NULL
-#define NULL        JsonNull();
+#define NULL        JsonNull()
 
-#define TRUE        true
-#define FALSE       false
+// Macros that call members
+#define HAS_KEY (var,key)   var.hasKey(key) 
+#define SIZE_OF (var)       var.sizeof()
+#define IS_EMPTY(var)       var.isEmpty()
+#define TYPE_OF (var)       var.typeOf()
 
 // Define a common interface for all Objects
 class JsonVariable {
 public:
     virtual             ~JsonVariable() = default;
-    virtual std::string typeOf() const  = 0;  
+
+    // They are different in every class
+    virtual std::string typeOf()        const {return "";}  
+    virtual std::string valueToString() const {return "";}
 
     // Mostly the same in all of the derived classes
     virtual bool        isEmpty() const { return 1; }
     virtual int         sizeOf()  const { return 1; }
-    virtual bool        hasKey()  const { return false; }
+    virtual bool        hasKey(std::string key)  const { return false; }
 };
 
 class JsonString : public JsonVariable {
 public:
     // Constructors
     JsonString(std::string value): value_(value){}    
-    std::string typeOf() const { return "string";}
+
+    // String Specific functions
+    std::string typeOf()        const { return "string";}
+    std::string valueToString() const { return value_;}
 private:
     std::string value_;
 };
@@ -45,7 +58,10 @@ class JsonNumber : public JsonVariable {
 public:
     // Constructors
     JsonNumber(double value): value_(value){}    
-    std::string typeOf() const { return "number";}
+
+    // Number Specific functions
+    std::string typeOf()        const { return "number";}
+    std::string valueToString() const { return std::to_string(value_);}
 private:
     double value_;
 };
@@ -54,7 +70,10 @@ class JsonBoolean : public JsonVariable {
 public:
     // Constructors
     JsonBoolean(bool value): value_(value){}    
-    std::string typeOf() const { return "boolean";}
+
+    // Boolean Specific functions
+    std::string typeOf()        const { return "boolean";}
+    std::string valueToString() const { return std::to_string(value_);}
 private:
     bool value_;
 };
@@ -75,7 +94,8 @@ private:
 
 class JsonNull : public JsonVariable {
 public:
-    std::string typeOf() const { return "null";}
+    std::string typeOf()        const { return "null";}
+    std::string valueToString() const { return "null";}
 };
 
 
--- src/json_dsl.cpp
+++ src/json_dsl.cpp
@@ -1,6 +1,9 @@
 #include <json_dsl.hpp>
 
-int main(void) { 
-    JSON(obj) = STRING("hello");
-    return 0; 
-}
+PROGRAM_BEGIN
+    JSON(obj1) = STRING("hello")
+    JSON(obj2) = NULL
+    JSON(obj3) = TRUE
+    JSON(obj4) = FALSE
+    JSON(obj5) = NUMBER(3.324);
+PROGRAM_END
